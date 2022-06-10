f289efd6d6554f042a8df386ce55f53ad8141093
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-06-10 14:24:56 -0500

    ControlFlow exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String message = "We don't need no education\n";
+        System.out.println(message);
+        message = "We don't need no thought control\n";
+        System.out.println(message);
+        message = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(message);
+        message = "In windows, the main drive is usually C:\\";
+        System.out.println(message);
+        message = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslashes \\\\\\!";
+        System.out.println(message);
+    }
+}
