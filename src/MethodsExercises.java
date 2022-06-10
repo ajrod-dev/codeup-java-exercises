538ae9485f91288bcc7f93da8f53e92a2075dce3
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-06-10 16:26:54 -0500

    MethodsExercise update

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,7 +1,7 @@
 public class MethodsExercises {
     public static void main(String[] args) {
         int x = 20;
-        int y = 7;
+        int y = 3;
         addition(x, y);
         subtraction(x,y);
         multiplication(x,y);
@@ -26,12 +26,22 @@ public class MethodsExercises {
     }
 
     public static Integer division(int x, int y){
-        System.out.println(x + " / " + y + " = " + (x / y));
-        return x / y;
+        if(y == 0){
+            System.out.println("Can't divide by 0");
+            return 0;
+        }else{
+            System.out.println(x + " / " + y + " = " + (x / y));
+            return x / y;
+        }
     }
 
     public static Integer modulus(int x, int y){
-        System.out.println(x + " % " + y + " = " + (x % y));
-        return x % y;
+        if(y == 0){
+            System.out.println("Can't divide by 0");
+            return 0;
+        }else{
+            System.out.println(x + " % " + y + " = " + (x % y));
+            return x % y;
+        }
     }
 }
