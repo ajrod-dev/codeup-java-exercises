cd1569cf3a44b7b20695a557d6b4f41994b17680
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-06-17 16:24:06 -0500

    update to MoviesApp exercise

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -8,34 +8,64 @@ public class MoviesApplication {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Movie[] movieArr = getAllMovies();
-        System.out.println("Welcome to Movie Application.");
-        System.out.println("What would you like to do? \n");
-        System.out.println("0 - exit");
-        System.out.println("1 - view all movies");
-        System.out.println("2 - view movies in the animated category");
-        System.out.println("3 - view movies in the drama category");
-        System.out.println("4 - view movies in the horror category");
-        System.out.println("5 - view movies in the scifi category\n");
-        System.out.println("Enter your choice: ");
-//        int input = sc.nextInt();
-//
-//        switch(input){
-//            case 0:
-//                System.out.println("Thanks for coming");
-//                System.exit(0);
-//                break;
-//            case 1:
-//                for(Movie movie : movieArr){
-//                    System.out.println(movie.getName());
-//                }
-//                break;
-//            case 2:
-//                for(Movie movie : movieArr){
-//                    System.out.println();
-//                }
-//
-//        }
+        System.out.println("Welcome to Movie Application.\n");
+        // Loop to run until user presses 0
+        boolean flag = true;
+        while(flag){
+            // Menu
 
+            System.out.println("------------------");
+            System.out.println("What would you like to do? \n\n");
+            System.out.println("0 - exit");
+            System.out.println("1 - view all movies");
+            System.out.println("2 - view movies in the animated category");
+            System.out.println("3 - view movies in the drama category");
+            System.out.println("4 - view movies in the horror category");
+            System.out.println("5 - view movies in the scifi category\n");
+            System.out.println("Enter your choice: \n");
+            int input = sc.nextInt();
+            switch(input) {
+                case 0:
+                    System.out.println("Thanks for coming");
+                    flag = false;
+                    break;
+                case 1:
+                    System.out.println("------------------");
+                    System.out.println("All Movies: ");
+                    System.out.println("------------------");
+                    for (Movie movie : movieArr) {
+                        System.out.println(movie.getName());
+                    }
+                    break;
+                case 2:
+                    System.out.println("------------------");
+                    System.out.println("Animated Movies: ");
+                    System.out.println("------------------");
+                    getMovieByGenre("animated");
+                    break;
+                case 3:
+                    System.out.println("------------------");
+                    System.out.println("Drama Movies: ");
+                    System.out.println("------------------");
+                    getMovieByGenre("drama");
+                    break;
+                case 4:
+                    System.out.println("------------------");
+                    System.out.println("Horror Movies: ");
+                    System.out.println("------------------");
+                    getMovieByGenre("horror");
+                    break;
+                case 5:
+                    System.out.println("------------------");
+                    System.out.println("Sci-Fi Movies: ");
+                    System.out.println("------------------");
+                    getMovieByGenre("scifi");
+                    break;
+                default:
+                    System.out.println("Thanks for coming by.");
+                    flag = false;
+                }
+            }
 
 
 
@@ -52,21 +82,15 @@ public class MoviesApplication {
          return returnedArr;
     }
 
-    public static Movie[] getMovieByGenre(String genre){
-        int count = 0;
+    public static void getMovieByGenre(String genre) {
         Movie[] result = MoviesArray.findAll();
-        for(Movie movie : result){
-            if(movie.getCategory().equalsIgnoreCase(genre)){
-                count++;
+        for (int i = 0; i < result.length; i++) {
+            if (!(result[i].getCategory().equals(genre))) {
+                continue;
+            } else {
+                System.out.println(result[i].getName());
             }
         }
-        Movie[] returnedArr = new Movie[count];
-        for(Movie movie : result){
-            if(movie.getCategory().equalsIgnoreCase(genre)){
-                returnedArr = Arrays.copyOf(result, result.length);
-            }
-        }
-        return returnedArr;
     }
 
 }
