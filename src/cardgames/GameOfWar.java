0e786d96bf44b5a45cdb04553682af6fa8791423
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-06-20 09:53:37 -0500

    update to extra_java_oop exercises

---
+++ src/cardgames/GameOfWar.java
@@ -0,0 +1,9 @@
+package cardgames;
+
+public class GameOfWar {
+    private int rounds = 26;
+
+    public static void main(String[] args) {
+        System.out.println("Game of War");
+    }
+}
---
+++ src/cardgames/Player.java
@@ -0,0 +1,12 @@
+package cardgames;
+
+public class Player {
+    private int gamesWon;
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+
+}
