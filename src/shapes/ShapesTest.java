01146de4fe72e89f56e4f84e5b708ff8fe503cc5
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-06-21 10:56:44 -0500

    update to abstract/interface exercises

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -6,7 +6,14 @@ public class ShapesTest {
     public static void main(String[] args) {
 
         Measurable myShape = new Square(4);
+        Measurable myShape2 = new Rectangle(9,10);
+
         System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+        System.out.println("---------------------");
+        System.out.println(myShape2.getArea());
+        System.out.println(myShape2.getPerimeter());
+
 
 
     }
